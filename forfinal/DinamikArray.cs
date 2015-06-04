3f3b1df40e649a745244ef58ab9a091ba89ebb6f
Aytaç Yıldız <aytac.yldz@gmail.com>
2015-06-04 18:38:51 +0300

    final sorulari icin

---
+++ forfinal/DinamikArray.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DynamicArray
+{
+	class MainClass
+	{
+		static int[] array;
+		static int capacity,size;
+
+		public static void Main (string[] args)
+		{
+			
+			Console.WriteLine ("number:");
+			string input = Console.ReadLine ();
+			int n = Convert.ToInt32 (input);
+			Random rand = new Random ();
+			capacity = 4;
+			array = new int[capacity];
+			for (size = 0; size < n; size++) {
+				if (size > (capacity - 1))
+					resize ();
+				array [size] = rand.Next();
+			}
+			print ();
+		}
+
+		static void resize ()
+		{
+			capacity *= 2;
+			int[] temp = new int[capacity];
+			for (int i = 0; i < size; i++) {
+				temp[i]=array[i];
+			}
+			array = temp;
+			Console.WriteLine ("resized:{0}",capacity);
+		}
+
+		static void print ()
+		{
+			for (int i = 0; i < size; i++) {
+				Console.Write (array[i] + " ");
+			}
+			Console.WriteLine ();
+		}
+	}
+}
---
+++ forfinal/UcluAramaAgaci.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace UcluAramaAgaci
+{
+	public class Node{
+		public int? lvalue,rvalue;
+		public Node left,right,middle;
+	}
+	public class TBTree{
+		public Node node;
+		/////////////////////////////////////////////////////////////////////////////////////////////////////////
+		//
+		// arama algoritması
+		//
+		public Node search(int key, Node node){
+			if (node == null || node.lvalue == key || node.rvalue == key)
+				return node;
+			if (node.lvalue != null && key < node.lvalue)
+				return search (key, node.left);
+			else if (node.lvalue != null && node.rvalue != null && key > node.lvalue && key < node.rvalue)
+				return search (key, node.middle);
+			else if (node.rvalue != null && key > node.rvalue)
+				return search (key, node.right);
+			else
+				return null;
+		}
+		/////////////////////////////////////////////////////////////////////////////////////////////////////////
+	}
+
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			// manuel inserts
+			TBTree tbt = new TBTree();
+			tbt.node = new Node ();
+			tbt.node.lvalue = 3;
+			tbt.node.rvalue = 9;
+			tbt.node.left = new Node ();
+			tbt.node.left.lvalue = 2;
+			tbt.node.middle = new Node ();
+			tbt.node.middle.lvalue = 6;
+			tbt.node.middle.left = new Node ();
+			tbt.node.middle.left.lvalue = 5;
+
+			Console.WriteLine (tbt.search(5,tbt.node).lvalue);
+			Console.WriteLine (tbt.search(5,tbt.node).rvalue);
+		}
+	}
+	
+}
